ded359d60519eb75016848677ed8fdaceb422aaa
prith46 <prithiviraj17a@gmail.com>
2024-06-03 17:41:58 +0530

    api

--- Weather_API_Testing/API_request.py
+++ Weather_API_Testing/API_request.py
@@ -1,2 +1,6 @@
-import time
-time.sleep(100000)
+import requests
+
+response = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/['
+                        'Puducherry,India]/1997-06-04/?key=68G7LZHXXUU6QR8QR2Q226C55')
+data = response.json()
+print(data['days'][0]['hours'][17]['temp'])
